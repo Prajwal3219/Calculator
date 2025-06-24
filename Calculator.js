f62ace6d6a0c4438b4bbc9d7217366759ffe7a2c
Prajwal3219 <prajvalkoli3@gmail.com>
2025-06-24 09:58:46 +0530

    js code

---
+++ Calculator.js
@@ -0,0 +1,9 @@
+// let display = document.querySelector(".display");
+// let r1 = document.querySelectorAll(".r1");
+// let para =document.querySelector("#p1");
+// let clr = document.querySelector("#ac");
+
+// r1.addEventListener("click",()=>{ 
+//     console.log("it is working");
+    
+// });
